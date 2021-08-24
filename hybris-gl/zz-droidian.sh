f51e8afb0a79289fd6544145c053ff11167f2bc2
Eugenio Paolantonio (g7) <me@medesimo.eu>
2021-08-24 22:26:24 +0200

    [droidian-quirks-hybris-gl] Disable webkit sandbox and compositing for now
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- hybris-gl/zz-droidian.sh
+++ hybris-gl/zz-droidian.sh
@@ -5,3 +5,9 @@ export COGL_DISABLE_MAPBUFFERRANGE=true
 
 # Force gles on gdk
 export GDK_GL=gles
+
+# Disable webkitgtk bwrap sandbox for now
+export WEBKIT_FORCE_SANDBOX=0
+
+# Disable webkit compositing for now
+export WEBKIT_DISABLE_COMPOSITING_MODE=1
