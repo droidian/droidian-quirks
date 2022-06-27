65ddf92e24bdc2fca4ba843b2212233cffdc0d56
Marcel Alexandru Nitan <nitan.marcel@protonmail.com>
2022-06-27 16:06:29 +0300

    [droidian-quirks-device] Also enable wlan on qualcomm and mediatek devices without boot_wlan

--- device/droidian-boot-wlan.sh
+++ device/droidian-boot-wlan.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-echo 1 > /sys/kernel/boot_wlan/boot_wlan
+[ -e /sys/kernel/boot_wlan/boot_wlan ] && echo 1 > /sys/kernel/boot_wlan/boot_wlan
+
+[ -e /dev/wlan ] && echo ON > /dev/wlan
+
+[ -e /dev/wmtWifi ] && echo 1 > /dev/wmtWifi
 
 exit 0
