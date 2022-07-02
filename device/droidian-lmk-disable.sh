53a8b69ef67752b140d903e38772b1fbe069036c
Eugenio Paolantonio (g7) <me@medesimo.eu>
2022-07-02 15:44:31 +0200

    [droidian-quirks-device] Adjust adj and minfree as well
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- device/droidian-lmk-disable.sh
+++ device/droidian-lmk-disable.sh
@@ -3,7 +3,9 @@
 # Disable lowmemorykiller for now
 #
 
-[ -e /sys/module/lowmemorykiller/parameters/enable_lmk ] echo 0 > /sys/module/lowmemorykiller/parameters/enable_lmk
+[ -e /sys/module/lowmemorykiller/parameters/adj ] && echo 9999 > /sys/module/lowmemorykiller/parameters/adj
+[ -e /sys/module/lowmemorykiller/parameters/minfree ] && echo 1 > /sys/module/lowmemorykiller/parameters/minfree
+[ -e /sys/module/lowmemorykiller/parameters/enable_lmk ] && echo 0 > /sys/module/lowmemorykiller/parameters/enable_lmk
 [ -e /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk ] && echo 0 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
 
 exit 0
