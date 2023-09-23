a93e3f62ba436bf8cdc25833e782086ee96d2502
Eugenio Paolantonio (g7) <me@medesimo.eu>
2023-09-23 16:34:13 +0200

    [hybris-gl] Disable webkitgtk DMA-BUF renderer
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- hybris-gl/zz-droidian.sh
+++ hybris-gl/zz-droidian.sh
@@ -9,3 +9,6 @@ export GDK_GL=gles
 # Disable webkitgtk bwrap sandbox for now
 export WEBKIT_FORCE_SANDBOX=0
 export WEBKIT_DISABLE_SANDBOX_THIS_IS_DANGEROUS=1
+
+# Disable webkitgtk DMA-BUF renderer
+export WEBKIT_DISABLE_DMABUF_RENDERER=1
