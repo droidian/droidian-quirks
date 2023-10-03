e1426a7e19843f671af5bdc66f77fb46392920e7
Bardia Moshiri <fakeshell@bardia.tech>
2023-10-03 15:35:44 -0400

    gnome: use the phosh version of CURRENT_DESKTOP and SESSION_DESKTOP

--- gnome/zz-gnome-vars.sh
+++ gnome/zz-gnome-vars.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 # some gnome apps depend on these
-export XDG_CURRENT_DESKTOP=GNOME
+export XDG_CURRENT_DESKTOP=Phosh:GNOME
+export XDG_SESSION_DESKTOP=phosh
 export XDG_MENU_PREFIX="gnome-"
+export QT_QPA_PLATFORMTHEME='gnome'
