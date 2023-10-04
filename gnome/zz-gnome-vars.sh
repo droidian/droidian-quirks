cb2186acc2cef82651d1a648ea58af6d7fa6a933
Bardia Moshiri <fakeshell@bardia.tech>
2023-10-03 21:57:05 -0400

    gnome: add adwaita style override for QT

--- gnome/zz-gnome-vars.sh
+++ gnome/zz-gnome-vars.sh
@@ -4,4 +4,7 @@
 export XDG_CURRENT_DESKTOP=Phosh:GNOME
 export XDG_SESSION_DESKTOP=phosh
 export XDG_MENU_PREFIX="gnome-"
+
+# make qt apps more gnome/gtk like
 export QT_QPA_PLATFORMTHEME='gnome'
+export QT_STYLE_OVERRIDE=adwaita
