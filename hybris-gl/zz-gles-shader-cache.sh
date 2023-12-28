7f7a1d0b9888f6b5df36eb2bca588dd714a84f5d
Jesús Higueras <jesus@dabbleam.com>
2023-12-28 21:20:33 +0100

    [droidian-quirks-hybris-gl] Add libglesshadercache.so to LD_PRELOAD
    When available, preloading this library cuts GTK4 app startup time in half
    or less.

---
+++ hybris-gl/zz-gles-shader-cache.sh
@@ -0,0 +1,17 @@
+LIBRARY="libglesshadercache.so"
+
+case "${LD_PRELOAD}" in
+	*${LIBRARY}*)
+		# Already there
+		return 0
+		;;
+	"")
+		;;
+	*)
+		LIBRARY="${LIBRARY}:"
+		;;
+esac
+
+if [ "${UID}" != "0" ]; then
+	export LD_PRELOAD="${LIBRARY}${LD_PRELOAD}"
+fi
