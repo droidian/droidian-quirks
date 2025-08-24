bd9e8d2bef743a009564ebd7994d0ee411808c09
Eugenio Paolantonio (g7) <me@medesimo.eu>
2025-08-24 23:41:38 +0200

    [droidian-quirks] Add droidian-quirks-firefox-gpu
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

---
+++ firefox-gpu/hybris-webrender.js
@@ -0,0 +1,18 @@
+// Copyright 2023-2025 Eugenio "g7" Paolantonio <me@medesimo.eu>
+// SPDX-License-Identifier: MPL-2.0
+
+// webrender compositor
+pref('gfx.webrender.compositor', true);
+pref('gfx.webrender.compositor.force-enabled', true);
+pref('gfx.webrender.compositor.fallback.software', false);
+
+// webrender misc
+pref('gfx.webrender.precache-shaders', true);
+pref('gfx.webrender.allow-partial-present-buffer-age', false);
+
+// layers
+// note: Droidian <= 100 *disabled* layers acceleration.
+// this has been changed since Droidian 101
+pref('layers.acceleration.force-enabled', true);
+pref('layers.gpu-process.enabled', true);
+pref('layers.gpu-process.force-enabled', true);
