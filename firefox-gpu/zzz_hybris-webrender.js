1b39afa9df160df7c587a55e73e91e0b00b4210a
Eugenio Paolantonio (g7) <me@medesimo.eu>
2025-09-06 10:44:03 +0200

    [droidian-quirks-firefox-gpu] Make it an add-on quirk rather than alternative
    Eases updates from Droidian 100 and lower.
    
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- firefox-gpu/zzz_hybris-webrender.js
+++ firefox-gpu/zzz_hybris-webrender.js
@@ -1,6 +1,7 @@
 // Copyright 2023-2025 Eugenio "g7" Paolantonio <me@medesimo.eu>
 // SPDX-License-Identifier: MPL-2.0
 
+
 // webrender compositor
 pref('gfx.webrender.compositor', true);
 pref('gfx.webrender.compositor.force-enabled', true);
@@ -13,6 +14,7 @@ pref('gfx.webrender.allow-partial-present-buffer-age', false);
 // layers
 // note: Droidian <= 100 *disabled* layers acceleration.
 // this has been changed since Droidian 101
+pref("layers.acceleration.disabled", false);
 pref('layers.acceleration.force-enabled', true);
 pref('layers.gpu-process.enabled', true);
 pref('layers.gpu-process.force-enabled', true);
